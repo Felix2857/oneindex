c7b15e2f13ac313edd49bc9aaa7116df95451068
N-W.Felix <36300397+Felix2857@users.noreply.github.com>
2020-02-15 12:54:42 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# oneIndex
+
+只修改了nextmoe主题
+
+donwa：https://github.com/donwa/oneindex
+davia7207：https://github.com/david7207/oneindexMod
---
+++ view/nexmoe/404.php
@@ -0,0 +1,5 @@
+<?php view::layout('layout')?>
+
+<?php view::begin('content');?>
+	<div class="mdui-typo-display-4" style="margin: 100px auto;text-align: center;">啊哦，什么也没找到.</div>
+<?php view::end('content');?>
